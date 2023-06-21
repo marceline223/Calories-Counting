1b7a7ce198b6d110c603b9c1165a13efa6ad045b
Dasha <dasha220301@gmail.com>
2023-06-21 12:36:56 +0300

    Add routing to header component

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
+    <div class="container-fluid justify-content-start">
+      <router-link to="/main" class="navbar-brand">
+        <img src="/logo.png" alt="ЗОЖ-Учёт" width="30">
+      </router-link>
+      <div>
+        <router-link to="/main" class="mx-3">Главная</router-link>
+        <router-link to="/statistic" class="mx-3">Статистика</router-link>
+        <router-link to="/product-list" class="mx-3">Справочник</router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+
+
+export default {
+  name: "Header"
+}
+
+</script>
+
+<style scoped>
+  a {
+    text-decoration: none;
+    color: black;
+    font-weight: bold;
+    font-size: large;
+  }
+
+
+</style>
